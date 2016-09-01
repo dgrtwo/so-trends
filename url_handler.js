96efd545d1029056a27e1ef20afd85a13ad75f83
dgrtwo <dgrtwo@princeton.edu>
2016-09-01 10:31:43 -0400

    Fixed URL

--- url_handler.js
+++ url_handler.js
@@ -3,7 +3,7 @@
 // http://stackoverflow.com/questions/25306519/shiny-saving-url-state-subpages-and-tabs
 Shiny.addCustomMessageHandler('setURL',
     function(data) {
-        window.history.pushState('object or string', 'Title', '/?tags=' + data.tags);
+        window.history.pushState('object or string', 'Title', '/so-trends/?tags=' + data.tags);
     }
 );
 </script>
